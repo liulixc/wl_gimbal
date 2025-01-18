bfccdf47a7d1d904103ef25b959065bc9d10b81b
liulixc <202300201053@stumail.sztu.edu.cn>
2025-01-18 19:50:17 +0800

    删除cmake文件

--- omni-infantry/cmake-build-debug/CMakeFiles/omni-infantry.elf.dir/bsp/bsp_i2c.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/omni-infantry.elf.dir/bsp/bsp_i2c.c.obj: \
- C:\Users\liuli\Desktop\wl_gimbal\omni-infantry\bsp\bsp_i2c.c
--- omni-infantry/cmake-build-debug/CMakeFiles/omni-infantry.elf.dir/component/devices/test.c.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/omni-infantry.elf.dir/component/devices/test.c.obj: \
- C:\Users\liuli\Desktop\wl_gimbal\omni-infantry\component\devices\test.c
